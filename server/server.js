ea10c279869560120d92fce02b1f2eb77ac01b33
satyamnitk <satyamanand180@gmail.com>
2023-07-06 01:55:26 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("./db");
+const connectDB = require("./database/db");
 const colors = require("colors");
 const userRoute = require("./routes/userRoute");
 const cookieParser = require("cookie-parser");
-const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -15,19 +14,6 @@ app.use(cors());
 connectDB();
 
 app.use("/api/user", userRoute);
-
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running successfully.");
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
