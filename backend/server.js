c3528c331dcb05a7bf1f26bafbcadc1af495335f
satyamnitk <135585598+satyamnitk@users.noreply.github.com>
2023-09-01 11:59:24 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./database/db");
+const colors = require("colors");
+const userRoutes = require("./route/userRoutes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+connectDB();
+
+app.use("/api/user", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`.yellow.bold);
+});
